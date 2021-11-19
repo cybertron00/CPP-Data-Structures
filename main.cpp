53ca5bb225c2361296e4a75100f37ad52d5fd633
Arko Banerjee <cybertron00@gmail.com>
2021-11-19 09:10:44 +0530

    main driver code.

--- main.cpp
+++ main.cpp
@@ -1,26 +1,6 @@
 #include <stdio.h>
+# include <iostream>
 
 int main() {
-
-    int option;
-    char choice;
-
-    while(1)
-    {
-        printf("\nPress 1 for Basic Coco Model ");
-        printf("\nPress 2 for Intermediate Coco Model ");
-        printf("\nPress 3 for Detail Coco Model \n");
-        scanf("%d", &option);
-
-        printf("\nYou have entered: %d", option);
-        printf("\nEnter y for confirm or n for retry: ");
-        scanf(" %c", &choice);
-        if (choice == 'y')
-            break;
-        //printf("\n");
-    }
-
-    printf("Your entered choice is: %d", option);
-
-    return 0;
+    std::cout << "Running C++ Data Structures...";
 }
